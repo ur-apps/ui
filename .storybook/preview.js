0724794ae7cf2a14903eede4e7b4ae6a56db5116
Garik <garikfreedomyan@gmail.com>
2022-01-20 04:08:09 +0300

    edited storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
+import React from 'react';
+
+import { ThemeProvider } from '../src/context';
 import '../src/scss/main.scss';
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    );
+  },
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -7,5 +20,25 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  backgrounds: {
+    values: [
+      {
+        name: 'White',
+        value: '#fff',
+      },
+      {
+        name: 'Light gray',
+        value: '#f5f5f5',
+      },
+      {
+        name: 'Dark gray',
+        value: '#0f0f0f',
+      },
+      {
+        name: 'Black',
+        value: '#000',
+      },
+    ],
   },
 };
