26ae0a296d92ed56d46615a92391c36de9149f1e
Garik <garikfreedomyan@gmail.com>
2022-01-30 16:34:10 +0300

    created Sandwich component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from './button';
 export * from './input';
 export * from './input-with-button';
+export * from './sandwich';
 export * from './select';
 export * from './theme-toggle';
 export * from './toggle';
---
+++ src/components/sandwich/sandwich.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import { useTheme } from 'context';
+import { classNames } from 'utils';
+import styles from './sandwich.module.scss';
+
+export interface ISandwichProps {
+  className?: string;
+  colorScheme?: 'blue' | 'white' | 'black';
+  autoColor?: boolean;
+  onClick?: () => void;
+}
+
+Sandwich.defaultProps = {
+  colorScheme: 'blue',
+  autoColor: true,
+} as ISandwichProps;
+
+export function Sandwich({ className, colorScheme, autoColor, onClick }: ISandwichProps) {
+  const { theme } = useTheme();
+
+  const clickHandler = () => {
+    onClick && onClick();
+  };
+
+  return (
+    <button
+      className={classNames(
+        styles.sandwich,
+        styles[`sandwich--${colorScheme}`],
+        styles[`sandwich--${theme}`],
+        autoColor ? styles[`sandwich--${theme}-mode`] : undefined,
+        className
+      )}
+      onClick={clickHandler}>
+      <span
+        className={classNames(
+          styles.line,
+          styles['line--top'],
+          styles[`line--${colorScheme}`],
+          styles[`line--${theme}`],
+          autoColor ? styles[`line--${theme}-mode`] : undefined
+        )}></span>
+      <span
+        className={classNames(
+          styles.line,
+          styles['line--center'],
+          styles[`line--${colorScheme}`],
+          styles[`line--${theme}`],
+          autoColor ? styles[`line--${theme}-mode`] : undefined
+        )}></span>
+      <span
+        className={classNames(
+          styles.line,
+          styles['line--bottom'],
+          styles[`line--${colorScheme}`],
+          styles[`line--${theme}`],
+          autoColor ? styles[`line--${theme}-mode`] : undefined
+        )}></span>
+    </button>
+  );
+}
