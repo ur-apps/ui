6a2e212ca7ef0f286d31db7348ae7935b72f8f9a
Garry Freeman <garryfreeman09@gmail.com>
2024-05-03 03:29:24 +0300

    feat: created Option component | #3

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './button';
 export * from './input';
+export * from './option';
 export * from './portal';
 export * from './text';
---
+++ src/components/option/option.tokens.ts
@@ -0,0 +1,89 @@
+import { IOptionStyleTokens } from './option.types';
+
+export const optionStyleTokens: IOptionStyleTokens = {
+  color: {
+    bg: {
+      filled: {
+        default: 'var(--uu-color-neutral-100)',
+        hover: 'var(--uu-color-neutral-150)',
+        selected: 'var(--uu-color-primary-500)',
+      },
+      outlined: {
+        default: 'var(--uu-color-bg-primary)',
+        hover: 'var(--uu-color-bg-primary)',
+        selected: 'var(--uu-color-bg-primary)',
+      },
+      clear: {
+        hover: 'var(--uu-color-neutral-a5)',
+      },
+    },
+    text: {
+      filled: {
+        default: 'var(--uu-color-text-primary)',
+        hover: 'var(--uu-color-text-primary)',
+        selected: 'var(--uu-color-text-contrast)',
+      },
+      outlined: {
+        default: 'var(--uu-color-text-primary)',
+        hover: 'var(--uu-color-text-primary)',
+        selected: 'var(--uu-color-primary-500)',
+      },
+      clear: {
+        default: 'var(--uu-color-text-primary)',
+        hover: 'var(--uu-color-text-primary)',
+        selected: 'var(--uu-color-primary-500)',
+      },
+    },
+    border: {
+      outlined: {
+        default: 'var(--uu-color-neutral-a15)',
+        hover: 'var(--uu-color-neutral-a25)',
+        selected: 'var(--uu-color-primary-500)',
+      },
+    },
+  },
+  height: {
+    s: 'var(--uu-height-s)',
+    m: 'var(--uu-height-m)',
+    l: 'var(--uu-height-l)',
+  },
+  'horizontal-padding': {
+    s: 'var(--uu-space-s)',
+    m: 'var(--uu-space-m)',
+    l: 'var(--uu-space-l)',
+  },
+  'border-radius': {
+    s: 'var(--uu-radius-xs)',
+    m: 'var(--uu-radius-s)',
+    l: 'var(--uu-radius-m)',
+  },
+  'font-size': {
+    s: 'var(--uu-font-size-xs)',
+    m: 'var(--uu-font-size-s)',
+    l: 'var(--uu-font-size-m)',
+  },
+  'line-height': {
+    s: 'var(--uu-line-height-xs)',
+    m: 'var(--uu-line-height-s)',
+    l: 'var(--uu-line-height-m)',
+  },
+  'font-weight': {
+    s: 'var(--uu-font-weight-500)',
+    m: 'var(--uu-font-weight-500)',
+    l: 'var(--uu-font-weight-500)',
+  },
+};
+
+export const optionDarkModeStyleTokens: IOptionStyleTokens = {
+  color: {
+    bg: {
+      filled: {
+        default: 'var(--uu-color-neutral-800)',
+        hover: 'var(--uu-color-neutral-750)',
+      },
+      clear: {
+        hover: 'var(--uu-color-neutral-a10)',
+      },
+    },
+  },
+};
