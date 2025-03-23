1b3a3f13c52930adbed95a27d032ecacca4c3b66
Garry Freeman <garryfreeman09@gmail.com>
2025-03-23 12:36:55 +0300

    fix: added global ignores for ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,8 @@
 import urappsCommonConfigs from '@ur-apps/common-fe/eslint';
 import storybookPlugin from 'eslint-plugin-storybook';
+import { globalIgnores } from 'eslint/config';
 
-export default urappsCommonConfigs.concat(storybookPlugin.configs['flat/recommended']);
+export default urappsCommonConfigs.concat(
+  storybookPlugin.configs['flat/recommended'],
+  globalIgnores(['node_modules/**/*', 'lib/**/*', 'storybook-static/**/*'])
+);
