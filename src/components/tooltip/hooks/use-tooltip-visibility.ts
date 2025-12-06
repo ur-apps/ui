c7131c44abc980612fd3aea9f4705fbe354bc24c
Garry Freeman <garryfreeman09@gmail.com>
2025-12-06 20:22:31 +0300

    feat(tooltip): add useTooltipVisibility hook for managing tooltip visibility

---
+++ src/components/tooltip/hooks/use-tooltip-visibility.ts
@@ -0,0 +1,68 @@
+import { useEffect } from 'react';
+import { useFlag, useHover } from '@ur-apps/common-fe';
+
+import { TooltipTrigger } from '../tooltip.types';
+
+export type UseTooltipVisibilityProps = {
+  trigger: TooltipTrigger;
+  targetRef: React.RefObject<HTMLElement | null>;
+  tooltipRef: React.RefObject<HTMLDivElement | null>;
+};
+
+export function useTooltipVisibility({ trigger, targetRef, tooltipRef }: UseTooltipVisibilityProps) {
+  const [isTooltipVisible, setIsTooltipVisible, toggleTooltipVisible] = useFlag(false);
+
+  const targetHovered = useHover(targetRef, {
+    enterDelay: 100,
+    leaveDelay: 200,
+    includeFocus: true,
+    pointerTypes: ['mouse', 'pen'],
+    disabled: trigger !== 'hover',
+  });
+  const tooltipHovered = useHover(tooltipRef, {
+    leaveDelay: 200,
+    disabled: trigger !== 'hover',
+  });
+
+  useEffect(() => {
+    if (trigger !== 'click') {
+      setIsTooltipVisible(false);
+    }
+  }, [trigger, setIsTooltipVisible]);
+
+  useEffect(() => {
+    if (trigger !== 'click' || !targetRef.current) return;
+
+    const target = targetRef.current;
+
+    target.addEventListener('click', toggleTooltipVisible);
+
+    return () => {
+      target.removeEventListener('click', toggleTooltipVisible);
+    };
+  }, [targetRef, toggleTooltipVisible, trigger]);
+
+  useEffect(() => {
+    if (trigger !== 'click' || !isTooltipVisible) return;
+
+    const target = targetRef.current;
+
+    const handleClickOutside = (event: MouseEvent) => {
+      const clickedElement = event.target as Node;
+      const isClickOnTooltip = tooltipRef.current?.contains(clickedElement);
+      const isClickOnTarget = target?.contains(clickedElement);
+
+      if (!isClickOnTooltip && !isClickOnTarget) {
+        setIsTooltipVisible(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside, true);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, [isTooltipVisible, setIsTooltipVisible, targetRef, tooltipRef, trigger]);
+
+  return trigger === 'manual' || tooltipHovered || targetHovered || isTooltipVisible;
+}
---
+++ src/components/tooltip/tooltip.types.ts
@@ -0,0 +1,1 @@
+export type TooltipTrigger = 'hover' | 'click' | 'manual';
