3d14e2c52edd25c7c93ed19e9648d43172846e1f
Garry Freeman <garryfreeman09@gmail.com>
2026-05-17 18:54:45 +0300

    feat: expand TextTag type to include additional HTML elements for greater flexibility

--- src/components/text/text.types.ts
+++ src/components/text/text.types.ts
@@ -3,7 +3,38 @@ import { TClasses } from '@ur-apps/common-fe';
 
 export type TextColor = 'inherit' | 'primary' | 'secondary' | 'contrast' | 'link' | 'link-visited';
 export type TextSize = 'xs' | 's' | 'm' | 'l' | 'xl';
-export type TextTag = 'p' | 'span';
+export type TextTag =
+  | 'a'
+  | 'abbr'
+  | 'address'
+  | 'b'
+  | 'bdi'
+  | 'bdo'
+  | 'blockquote'
+  | 'cite'
+  | 'code'
+  | 'data'
+  | 'del'
+  | 'dfn'
+  | 'div'
+  | 'em'
+  | 'i'
+  | 'ins'
+  | 'kbd'
+  | 'label'
+  | 'mark'
+  | 'p'
+  | 'q'
+  | 's'
+  | 'samp'
+  | 'small'
+  | 'span'
+  | 'strong'
+  | 'sub'
+  | 'sup'
+  | 'time'
+  | 'u'
+  | 'var';
 
 export interface ITextProps<T extends TextTag> extends Omit<
   HTMLAttributes<T extends 'span' ? HTMLSpanElement : HTMLParagraphElement>,
